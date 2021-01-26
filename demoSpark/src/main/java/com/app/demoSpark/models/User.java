ac617ef64a7548d8345d41e19b9cce60efa501f3
Mobilpymes <whuera@gmail.com>
2021-01-26 00:15:45 -0500

    add new data

--- demoSpark/src/main/java/com/app/demoSpark/models/User.java
+++ demoSpark/src/main/java/com/app/demoSpark/models/User.java
@@ -3,12 +3,15 @@ package com.app.demoSpark.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 @AllArgsConstructor
 public class User {
     private String id;
     private String firstName;
     private String lastName;
-    private String email;
+    private Optional<String> email;
+    private Optional<String> imageUrl;
     private UserBasicData userBasicData;
 }
--- demoSpark/src/main/java/com/app/demoSpark/sparkMain.java
+++ demoSpark/src/main/java/com/app/demoSpark/sparkMain.java
@@ -4,62 +4,76 @@ import com.app.demoSpark.models.User;
 import com.app.demoSpark.service.UserService;
 import com.app.demoSpark.service.UserServiceImpl;
 import com.google.gson.Gson;
-import static spark.Spark.delete;
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.put;
-import static spark.Spark.post;
 
+import static spark.Spark.*;
 
 
 public class sparkMain {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
-         UserService userService = new UserServiceImpl ();
+        UserService userService = new UserServiceImpl ();
 
         //configure port
-        port(8080);
+        port (8080);
 
-        get("/hello", (req, res)->"Hello, world!");
+        get ("/hello", (req, res) -> "Hello, world!");
 
-        get("/hello/:name", (req,res)->{
-            return "Hello, "+ req.params(":name") + "!!";
+        get ("/hello/:name", (req, res) -> {
+            return "Hello, " + req.params (":name") + "!!";
         });
 
-        post("/users/add", (req,res)-> {
-            res.type("application/json");
+        options ("/*",
+                (request, response) -> {
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+                    return "OK";
+                });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
-            User user = new Gson().fromJson(req.body(), User.class);
+        post ("/users/add", (req, res) -> {
+            res.type ("application/json");
+
+            User user = new Gson ().fromJson (req.body (), User.class);
             userService.addUSer (user);
-            return new Gson().toJson(user);
+            return new Gson ().toJson (user);
         });
 
-        get("/users", (req,res)-> {
-            res.type("application/json");
-            return new Gson().toJson(userService.getUsers());
+        get ("/users", (req, res) -> {
+            res.type ("application/json");
+            return new Gson ().toJson (userService.getUsers ());
         });
 
-        get("/users/:id", (req,res)-> {
-            res.type("application/json");
-            return new Gson().toJson(userService.getUserById (req.params (":id")));
+        get ("/users/:id", (req, res) -> {
+            res.type ("application/json");
+            return new Gson ().toJson (userService.getUserById (req.params (":id")));
         });
 
-        delete("/users/delete/:id", (req,res) -> {
-            res.type("application/json");
-            userService.deleteUserById(req.params(":id"));
-            return new Gson().toJson("User deleted");
+        delete ("/users/delete/:id", (req, res) -> {
+            res.type ("application/json");
+            userService.deleteUserById (req.params (":id"));
+            return new Gson ().toJson ("User deleted");
         });
 
-        put("/users/edit/:id", (req,res) -> {
-            res.type("application/json");
-            User ue = new Gson().fromJson(req.body(), User.class);
+        put ("/users/edit/:id", (req, res) -> {
+            res.type ("application/json");
+            User ue = new Gson ().fromJson (req.body (), User.class);
 
-            User searchedUser = userService.getUserById(ue.getId());
-            if(searchedUser != null) {
-                userService.editUser(ue);
-                return new Gson().toJson(ue);
+            User searchedUser = userService.getUserById (ue.getId ());
+            if (searchedUser != null) {
+                userService.editUser (ue);
+                return new Gson ().toJson (ue);
             } else {
-                return new Gson().toJson("User not found or error in edit");
+                return new Gson ().toJson ("User not found or error in edit");
             }
 
         });
